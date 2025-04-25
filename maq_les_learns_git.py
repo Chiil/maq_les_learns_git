c218c1fda02af6aae1d745eb4018643b0a0112c5
Chiel van Heerwaarden <chielvanheerwaarden@gmail.com>
2025-04-25 10:03:25 +0200

    Initial commit with tasks.

---
+++ maq_les_learns_git.py
@@ -0,0 +1,26 @@
+def print_message():
+    print("Hello wrold!")
+
+
+if __name__ == "__main__":
+
+    # Task 1: take two integers from the command line
+    # that will be the matrix arguments
+    # itot = ...
+    # jtot = ...
+
+    # Task 2: adjust the print message and show the itot and jtot input.
+    print_message()
+
+    # Task 3: generate np arrays x and y that go from 0 to 1 in itot and
+    # jtot steps.
+
+    # Task 4: generate a 2D np array a with random numbers
+
+    # Task 5: plot the array a with axes x and y using pcolormesh.
+
+    # Task 6: calculate the mean of the array a and print it to the screen.
+
+    # Task 7: calculate the variance of the array a and print it to the screen.
+
+
